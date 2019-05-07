531c30714271c6ae9e3721440f4ad3de97c763af
Jonathan Spruance (Insight Global) <v-jonspr@microsoft.com>
2019-05-07 11:56:03 -0700

    Adding hero cards and carousel

--- core-MultiDialogs-v4/dialogs/hotels.js
+++ core-MultiDialogs-v4/dialogs/hotels.js
@@ -1,3 +1,5 @@
+const { ActivityHandler, ActionTypes, ActivityTypes, AttachmentLayoutTypes, CardFactory } = require('botbuilder');
+const store = require('../store');
 const { 
     INITIAL_HOTEL_PROMPT, 
     CHECKIN_DATETIME_PROMPT, 
@@ -47,7 +49,19 @@ const hotelsDialog = [
         const destination = stepContext.values.destination;
         const checkIn = new Date(stepContext.values.checkinTime);
         const checkOut = addDays(checkIn, stepContext.values.numberofnights);
+
         await stepContext.context.sendActivity(`Ok. Searching for Hotels in ${destination} from ${checkIn.toDateString()} to ${checkOut.toDateString()}...`);
+        const hotels = await store.searchHotels(destination, checkIn, checkOut);
+        await stepContext.context.sendActivity(`I found in total ${hotels.length} hotels for your dates:`);
+        
+        const hotelHeroCards = hotels.map(createHotelHeroCard);
+
+        await stepContext.context.sendActivity({
+            attachments: hotelHeroCards,
+            attachmentLayout: AttachmentLayoutTypes.Carousel
+        });
+        
+        return await stepContext.endDialog();
     }
   
 ];
@@ -57,5 +71,20 @@ const addDays = (startDate, days) => {
     date.setDate(date.getDate() + days);
     return date;
 };
+
+const createHotelHeroCard = (hotel) => {
+    return CardFactory.heroCard(
+        hotel.name,
+        `${hotel.rating} stars. ${hotel.numberOfReviews} reviews. From ${hotel.priceStarting} per night.`,
+        CardFactory.images([hotel.image]),
+        CardFactory.actions([
+            {
+                type: 'openUrl',
+                title: 'More details',
+                value: `https://www.bing.com/search?q=hotels+in+${encodeURIComponent(hotel.location)}`
+            }
+        ])
+    );
+}
 
 exports.hotelsDialog = hotelsDialog;
